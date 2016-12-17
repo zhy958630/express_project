eb15177e316d6d639bcf9bd46f1bdbe66ce2fa25
zhy958630 <958630332@qq.com>
2016-12-17 22:38:48 +0800

    Moved 'fortune cookie' functionality into module

--- travel_agency_1/meadowlarktravel.js
+++ travel_agency_1/meadowlarktravel.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var fortune = require('./lib/fortune.js');
 var app = express();
 // 设置 handlebars 视图引擎
 var handlebars = require('express3-handlebars')
@@ -10,14 +10,7 @@ app.set('view engine','handlebars');
 //指定程序端口
 app.set('port',process.env.PORT || 3000);
 
-//幸运饼干数组
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-];
+
 
 //static 中间件
 app.use(express.static(__dirname + '/public'));
@@ -31,9 +24,9 @@ app.get('/', function(req, res){
 app.get('/about', function(req, res){
         // res.type('text/plain');
         // res.send('About Meadowlark Travel');
-        var randomFortune = 
-                fortunes[Math.floor(Math.random() * fortunes.length)];
-        res.render('about',{fortune:randomFortune});
+        // var randomFortune = 
+        //         fortunes[Math.floor(Math.random() * fortunes.length)];
+        res.render('about',{fortune:fortune.getFortune()});
 });
 
 // 定制404页面
